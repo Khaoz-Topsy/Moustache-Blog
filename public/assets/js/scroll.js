29cd220a2d672ac6f4aa3e3c7b1cba9203558a3e
Kurt Lourens <hi@kurtlourens.com>
2022-08-19 18:03:07 +0200

    ✨ Navbar with scroll effect

--- public/assets/js/scroll.js
+++ public/assets/js/scroll.js
@@ -1,8 +1,25 @@
-window.onscroll = function () { myFunction() };
+window.onscroll = function () { onScrollFunc() };
 
-function myFunction() {
+function onScrollFunc() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
+
+    var postNav = document.querySelector(".navbar.post");
+    if (postNav == null) return;
+
+    var shouldBeShown = scrolled > 0;
+    var hasShown = postNav.classList.contains('show');
+    if (shouldBeShown && hasShown == false) {
+        postNav.classList.add('show');
+    }
+
+    if (shouldBeShown == false && hasShown) {
+        postNav.classList.remove('show');
+    }
+
+    var postNavProgress = document.querySelector(".navbar.post .progress");
+    if (postNavProgress == null) return;
+
+    postNavProgress.style.width = scrolled + "%";
 }
